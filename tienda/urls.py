896e9e45d8e8ede5138e11c019e32506e20891fd
Brayan Potosi <bp990308@hotmail.com>
2020-11-17 01:26:17 -0500

    Bug Fixed

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,8 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from tienda import views
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
