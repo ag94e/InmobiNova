bc2e89034281827dea41af05a82196aa7b710718
Brayan Potosi <bp990308@hotmail.com>
2020-11-17 02:13:39 -0500

    Deploy debug

--- tienda/settings.py
+++ tienda/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9=#2+n=oey+addo=%upcc8n#1dlndjbwh8grdw#euvtp)tate4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
